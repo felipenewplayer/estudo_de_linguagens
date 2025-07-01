bed552592cf568010e93fd98c5d977893aeba35b
felipenewplayer <felipeapereira2014@gmail.com>
2025-07-01 16:36:14 -0300

    tipos_primitivo_PHP

---
+++ PHP/tipos_primitivos.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$nome = "Felipe";  // String
+$idade = 27; // Int
+$altura = 1.78; // Float
+$isworking = false; // Booleano
+$money = null; // Nullo kkkk
+
+
+//Fazendo print
+
+echo $nome . "\n";
+echo $idade . "\n";
+echo $altura . "\n";
+echo $isworking . "\n";
+echo $money . "\n";
